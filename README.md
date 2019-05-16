cc59d4830bd2c63984a91ca0e4cd67fc5c1b67bc
booil <booiljung@me.com>
2019-05-16 18:48:03 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# fgoomap
+
+Flutter Google Map Demo project.
+
+## About
+
+This project is a implementation of [Adding Google Maps to Flutter](https://medium.com/flutter-io/google-maps-and-flutter-cfb330f9a245)
+
+## Run Demo
+
+### For Android
+
+If you want to run this demo on your Android phone, Fix a API KEY at `./android/app/src/main/AndroidManifest.xml'` as your API KEY.
+
+### For iOS
+
+If you want to run this demo on your iPhone, Fix a API KEY at `./ios/Runner/AppDelegate.m'` as your API KEY.
+
+---
+
+Have a happy coding.
---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,18 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+
+// Add the GoogleMaps import.
+#import "GoogleMaps/GoogleMaps.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  [GMSServices provideAPIKey:@"YOUR IOS API KEY HERE"];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
